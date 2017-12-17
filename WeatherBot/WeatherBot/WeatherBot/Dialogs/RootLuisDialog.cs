de1d46483f319e7eae59066bcc64fb2988537095
Edson Silva <edson_nasilva@hotmail.com>
2017-12-16 22:58:26 -0200

    Versão inicial do projeto

---
+++ WeatherBot/WeatherBot/WeatherBot/Dialogs/RootLuisDialog.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using System.Linq;
+using WeatherBot.Services;
+
+namespace WeatherBot.Dialogs
+{
+    //[LuisModel("appId", "key")]
+    [LuisModel("36ae9d02-546c-46f9-868f-625cf9ab3859", "a6875a3811c547d0840f0a459f4f938a")]
+    [Serializable]
+    public class RootLuisDialog : LuisDialog<object>
+    {
+        [LuisIntent("")]
+        [LuisIntent("None")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            // Mensagem que o bot vai devolver ao usuário
+            string message = "Não entendi o que você quis dizer.";
+
+            // Avisa ao contexto do bot para ele mandar a mensagem para o usuário via POST
+            await context.PostAsync(message);
+
+            // Avisa ao contexto do bot para aguardar uma mensagem de resposta do usuário 
+            context.Wait(this.MessageReceived);
+        }
+                
+        [LuisIntent("Wheather_Intent")]
+        public async Task WeatherForecast(IDialogContext context, LuisResult result)
+        {
+            // Obtém o nome da cidade
+            var city = result.Entities.FirstOrDefault()?.Entity;
+
+            // Consulta a API com o nome da cidade
+            var forecast = await WeatherService.GetForescast(city);
+
+            // Mensagem que o bot vai devolver ao usuário
+            string message = $"Tempo em {city.First().ToString().ToUpper() + city.Substring(1)} é de {forecast?.weather?.FirstOrDefault().description} com mínima de {forecast?.main?.temp_min}º e máxima de {forecast?.main?.temp_max}º";
+
+            // Avisa ao contexto do bot para ele mandar a mensagem para o usuário via POST
+            await context.PostAsync(message);
+
+            // Avisa ao contexto do bot para aguardar uma mensagem de resposta do usuário 
+            context.Wait(this.MessageReceived);
+        }
+    }
+}
---
+++ WeatherBot/WeatherBot/WeatherBot/Services/WeatherService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Newtonsoft.Json;
+using WeatherBot.Model;
+
+namespace WeatherBot.Services
+{
+    public class WeatherService
+    {
+        private static string APIKey = "5e46e301d42528287fe3a46262e7a99b";
+        private static string APILanguage = "pt";
+
+        public async static Task<OpenWeather> GetForescast(string city)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri("http://api.openweathermap.org/");
+                HttpResponseMessage response = client.GetAsync($"data/2.5/weather?q={RemoveAccents(city)}&APPID={APIKey}&units=metric&lang={APILanguage}").Result;
+
+                var responseJson = await response.Content.ReadAsStringAsync();
+
+                OpenWeather result = JsonConvert.DeserializeObject<OpenWeather>(responseJson);
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private static string RemoveAccents(string text)
+        {
+            return new string(text
+                .Normalize(NormalizationForm.FormD)
+                .Where(ch => char.GetUnicodeCategory(ch) != UnicodeCategory.NonSpacingMark)
+                .ToArray());
+        }
+    }
+}
